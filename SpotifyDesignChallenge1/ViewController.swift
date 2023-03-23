d98ee9dd4cdf98fa57906489f1e509678409cc3a
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-23 14:21:33 -0500

    Initial Commit

---
+++ SpotifyDesignChallenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyDesignChallenge1
+//
+//  Created by Edwin Cardenas on 3/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
